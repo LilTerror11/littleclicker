35a23ce071f6cec565d4d94825adf1172569eab8
LilTerror11 <144447975+LilTerror11@users.noreply.github.com>
2024-03-15 12:05:14 +0930

    added loging stuff

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
-console.log("Little Clicker is starting!")
-console.log("Little Clicker by lilterror11, this is currently a Work In Progress")
+console.log("Little Clicker is starting!");
+console.log("Little Clicker by lilterror11, this is currently a Work In Progress");
 
-console.log("loading...")
+console.log("loading...");
 
-if (localStorage.money == undefined) {localStorage.money = 0}
-if (localStorage.clickPower == undefined) {localStorage.clickPower = 1}
-if (localStorage.autoClicker == undefined) {localStorage.autoClicker = 0}
+// temp variables
+console.log("loading... temp variables")
+
+let cv // Created Variables
+
+let cps // Clicks Per Second
+
+console.log("loading... temp variables, complete!")
+
+// localStorage loading
+console.log("loading... creating localStorage variables")
+
+if (localStorage.money == undefined) {localStorage.money = 0; cv++;} // clicks
+if (localStorage.clickPower == undefined) {localStorage.clickPower = 1; cv++;} // how much 'money' you get from a click
+if (localStorage.autoClicker == undefined) {localStorage.autoClicker = 0; cv++;} // realy fast click generation
+
+console.log("loading... creating localStorage variables, complete!")
+console.log("DEBUG: created " + toString(cv) + " variables")
+
+// creating temp variables for localStorage
+console.log("loading... creating temp variables for localStorage")
 
 money = Number(localStorage.money);
 clickPower = Number(localStorage.clickPower);
 autoClicker = Number(localStorage.autoClicker);
+
+console.log("loading... creating temp variables for localStorage, complete!")
+
+// functions
+
+console.log("loading... functions")
