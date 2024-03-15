4a2d380c1cfbcd684d42e081a6753a38beada960
LilTerror11 <144447975+LilTerror11@users.noreply.github.com>
2024-03-15 16:39:33 +0930

    testing

--- main.js
+++ main.js
@@ -4,23 +4,23 @@ console.log("Little Clicker by lilterror11, this is currently a Work In Progress
 console.log("loading...");
 
 // temp variables
-console.log("loading... temp variables")
+console.log("loading... temp variables");
 
 let cv = 0; // Created Variables
-
+let moneyInt = 0;
 let cps = 0; // Clicks Per Second
 
-console.log("loading... temp variables, complete!")
+console.log("loading... temp variables, complete!");
 
 // localStorage loading
-console.log("loading... creating localStorage variables")
+console.log("loading... creating localStorage variables");
 
-if (localStorage.money == undefined) {localStorage.money = 0; cv++;} // clicks
+if (localStorage.money == undefined) {localStorage.money = 0.0; cv++;} // clicks
 if (localStorage.clickPower == undefined) {localStorage.clickPower = 1; cv++;} // how much 'money' you get from a click
 if (localStorage.autoClicker == undefined) {localStorage.autoClicker = 0; cv++;} // realy fast click generation
 
-console.log("loading... creating localStorage variables, complete!")
-console.log("DEBUG: created " + String(cv) + " variables")
+console.log("loading... creating localStorage variables, complete!");
+console.log("DEBUG: created " + String(cv) + " variables");
 //for (let i = 0; i < localStorage.length; i++) {
 //    console.log(toString(localStorage));
 //}
@@ -28,17 +28,17 @@ console.log("DEBUG: created " + String(cv) + " variables")
 const items = { ...localStorage };
 
 // creating temp variables for localStorage
-console.log("loading... creating temp variables for localStorage")
+console.log("loading... creating temp variables for localStorage");
 
 money = Number(localStorage.money);
 clickPower = Number(localStorage.clickPower);
 autoClicker = Number(localStorage.autoClicker);
 
-console.log("loading... creating temp variables for localStorage, complete!")
+console.log("loading... creating temp variables for localStorage, complete!");
 
 // functions
 
-console.log("loading... functions")
+console.log("loading... functions");
 
 function saveStorage() {
     localStorage.money = money;
@@ -46,7 +46,12 @@ function saveStorage() {
     localStorage.autoClicker = autoClicker;
 }
 
+function autoClickers() {
+    money += auto;
+}
+
 setInterval(function() {
     cps++;
+    cps *= 1.0;
     document.getElementById("cps").innerHTML = cps;
 }, 100);
