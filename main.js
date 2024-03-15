5545083b34064282e86d8437b5f462f446a98566
LilTerror11 <144447975+LilTerror11@users.noreply.github.com>
2024-03-15 10:30:40 +0930

    .

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 console.log("Little Clicker is starting!")
 console.log("Little Clicker by lilterror11, this is currently a Work In Progress")
+
+console.log("loading...")
+
+if (localStorage.money == undefined) {localStorage.money = 0}
+if (localStorage.clickPower == undefined) {localStorage.clickPower = 1}
+if (localStorage.autoClicker == undefined) {localStorage.autoClicker = 0}
+
+money = Number(localStorage.money);
+clickPower = Number(localStorage.clickPower);
+autoClicker = Number(localStorage.autoClicker);
